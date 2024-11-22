55b3dab2d509bdb18a768987b7e024355c95f167
shokkontent <grmgorer@gmail.com>
2024-11-22 13:47:22 +0300

    Update database_description.py

--- database_description.py
+++ database_description.py
@@ -42,6 +42,11 @@ class MyWidget(QMainWindow):
         size_button = int((min(self.width(), self.height()) // 10) / 1.54)
         self.pushButton_2.setFixedSize(size_button + 25, size_button)
 
+        # Меняет изображение кнопку удаления
+        self.pushButton_3.setStyleSheet("border-image : url(pmg/plants2.jpg);")
+        size_button = int((min(self.width(), self.height()) // 10) / 1.54)
+        self.pushButton_3.setFixedSize(size_button + 25, size_button)
+        # заполняет tableWidget
         solution = (f'SELECT name FROM description')
         res = self.cur.execute(solution).fetchall()
         self.tableWidget.setRowCount(len(res))
@@ -49,17 +54,30 @@ class MyWidget(QMainWindow):
         for row, films in enumerate(res):
             for line, name in enumerate(films):
                 self.tableWidget.setItem(row, line, QTableWidgetItem(str(name)))
+        number = row
         # конектим tableWidget
         self.tableWidget.cellClicked.connect(self.perenos)
+
         # конектим pushButton
         self.pushButton.clicked.connect(self.perexod)
         # конектим pushButton_2
         self.pushButton_2.clicked.connect(self.dialog_box)
+        # конектим pushButton_3
+        self.pushButton_3.clicked.connect(self.delet)
 
     def perenos(self, row):
         self.label_2.setText(str(row + 1))
         with open('txt/peredacha.txt', 'w', encoding="utf-8") as f:
             f.write(str(row + 1))
+
+    def delet(self, row):
+        self.con = sqlite3.connect('db/lk.db')
+
+        self.cur = self.con.cursor()
+        sol = (f'DELETE FROM Description WHERE id = {row}')
+        self.cur.execute(sol)
+        self.con.commit()
+
 
     def dialog_box(self):
         subprocess.run(['python', 'dialog_box.py'])
